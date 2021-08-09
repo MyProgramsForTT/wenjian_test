3e1ee6c4a6b687031e3ceb7b99ca1be2328da8be
TT <70446332+MyProgramsForTT@users.noreply.github.com>
2021-08-09 17:04:22 +0800

    Create test.py

---
+++ api/test.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+from fastapi import FastAPI
+from typing import Dict
+app = FastAPI()
+
+data ='{"Web":"http://47.113.205.237/web/","Payload":"http://47.113.205.237/payload/","Playgame":"https://www.tomorrowing.cn/playgames/"}'
+app = FastAPI()
+
+@app.get("/end/", response_model=Dict[str, str ])
+async def read_keyword_weights():
+    return {"Web":"http://47.113.205.237/web/","Payload":"http://47.113.205.237/payload/","Playgame":"https://www.tomorrowing.cn/playgames/"}
+
+import os
+@app.get("/")
+
+async def read_keyword_weights():
+    name = ''
+    file_dir = 'C:/Users/kakarrote/Desktop/fidder/demo'
+    for root, dirs, files in os.walk(file_dir, topdown=False):
+        for i in files:
+            data=i+','
+            name+=data
+       
+
+    return name
